104a7fc4fc67f9838c526d4861e8691dd7d3c290
MartinRichtr <mindhackx@mindhackx.cz>
2025-04-02 02:04:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+
 
 export default function HomePage() {
   return (
